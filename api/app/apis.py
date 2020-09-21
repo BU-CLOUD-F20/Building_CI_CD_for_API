621a1590f935b7f29a864aa61e2351cb7e68972f
Mella Liang <mellaliang47@gmail.com>
2020-09-20 21:52:01 -1000

    added frame of api

--- api/app/apis.py
+++ api/app/apis.py
@@ -1,6 +1,28 @@
 from app import app
+from flask_restful import Resource, Api
 
+api = Api(app)
 
 @app.route("/")
 def index():
-    return "FLASK APP SETUP"
+    return "Welcome to Doubly!"
+
+class ShortLink(Resource):
+    def get(self, link):
+        shortLink = link
+        return {
+        'longLink': 'original url'
+        }
+    def post(self, link):
+        longLink = link
+        #algo for converting longLInk to ShortLink
+        return {
+        'shortLink': 'shortLink',
+        'expireAt': 'date'
+        }
+    def delete(self, link):
+        shortLink = link
+        #algo for deleting ShortLink
+        return 'deleted'
+
+api.add_resource(ShortLink, '/<string:link>')
