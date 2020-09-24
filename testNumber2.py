d88436b24be2f5e7ca3edf2c6db482a052fcf707
Kaito Yamagishi <kaitoyamagishi1109@users.noreply.github.com>
2020-09-24 19:51:42 +0000

    Automated autoyapf fixes

--- testNumber2.py
+++ testNumber2.py
@@ -1,19 +1,13 @@
-def is_unique( 
-               s 
-               ): 
-    s = list(s 
-                ) 
-    s.sort() 
-  
-  
-    for i in range(len(s) - 1): 
-        if s[i] == s[i + 1]: 
+def is_unique(s):
+    s = list(s)
+    s.sort()
+
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
             return 0
-    else: 
+    else:
         return 1
-  
-  
-if __name__ == "__main__": 
-    print( 
-          is_unique(input()) 
-         ) 
+
+
+if __name__ == "__main__":
+    print(is_unique(input()))
