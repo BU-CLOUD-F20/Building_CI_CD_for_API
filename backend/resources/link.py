162c24221772fe7fa32b299f70096eece0dce91f
Mella Liang <mellaliang47@gmail.com>
2020-09-24 22:48:17 -1000

    Made syntax changes and combined classes into Link Changed longLink to original_link for more intuitive variable name. Changed variable and function names to snake_case to follow Python convention. Combined ShortLink and LongLink classes into Link class.

--- backend/resources/link.py
+++ backend/resources/link.py
@@ -1,82 +1,71 @@
-# Import flask
+original_link# Import flask
 from flask import redirect, jsonify, request
 from flask_restful import Resource
-
 
 # Import libraries
 import string
 
 
-class ShortLink(Resource):
-    # redirect to
-    def get(self, link):
-        shortLink = link
-        # TODO
+class Link(Resource):
+    def get(self, link_id):
         # get original URL from DB
         exist = True  # check from DB if the short link exist in DB
         if exist:
             return redirect('https://www.youtube.com/')
-            # return {
-            # 'longLink': 'original url',
-            # }
+            # return redirect(original_link)
         return {
             'message': "The short link doesn't exist.",
-            'status': 404,
-        },
-
-
-class LongLink(Resource):
-    def get(self):
-        return "Long Link"
+            'status': 404
+        }
 
     def post(self):
         json_data = request.get_json(force=True)
         # json data looks like this: (to be modified)
         # {
-        #     "longLink": "https://www.youtube.com/",
-        #     "expireAt" : "2020/9/30"
+        #     "original_link": "https://www.youtube.com/",
+        #     "expire_at" : "2020/9/30"
         # }
-        longLink = json_data['longLink']
+        original_link = json_data['original_link']
         # checking URL validation
         try:
-            if 'http://' not in json_data['longLink'] or 'https://' not in json_data['longLink']:
-                longLink = 'http://' + longLink
-                print(longLink)
-            if '.com' not in json_data['longLink']:
-                longLink = longLink + '.com'
-                print(longLink)
-            response = request.get(longLink)
+            if 'http://' not in json_data['original_link'] or 'https://' not in json_data['original_link']:
+                original_link = 'http://' + original_link
+                print(original_link)
+            if '.com' not in json_data['original_link']:
+                original_link = original_link + '.com'
+                print(original_link)
+            response = request.get(original_link)
             print("URL is valid and exists on the internet")
-            shortLink = self.shortLinkGenerator()  # call to the short link generator
+            short_link = self.short_link_generator()  # call to the short link generator
             # TODO
-            # Map shortLink and json_data['longLink'] to DB
+            # Map short_link and json_data['original_link'] to DB
             return {
-                'shortLink': shortLink,
-                'expireAt': 'date',
+                'short_link': short_link,
+                'expire_at': 'date',
             }
         except request.ConnectionError as exception:
             return {
-                'message': json_data['longLink'] + " is not a valid URL",
+                'message': json_data['original_link'] + " is not a valid URL",
                 'status': 404,
             },
 
     def delete(self, link):
-        shortLink = link
+        short_link = link
         # TODO
-        # algo for deleting ShortLink
+        # algo for deleting short_link
         return 'deleted'
 
-    def shortLinkGenerator(self):
+    def short_link_generator(self):
         # TODO
         # Get INDEX of last url in the database. eg:
         index = 990000099999  # change this index to see what the algo looks like
         # index + 1 => the index of the new upcoming url
-        shortLink = self.encode(index + 1)  # 62^5 choices
-        return shortLink
+        short_link = self.encode(index + 1)  # 62^5 choices
+        return short_link
         # exist = False
         # if exist:
-        #     return self.shortLinkGenerator()
-        # return shortLink
+        #     return self.short_linkGenerator()
+        # return short_link
 
     def encode(self, index):  # from number to 62 bases eg: 1000000 => 15FTGg
         characters = string.digits + string.ascii_letters
