34bdabab1ed89532c60047b1b05e06acf0d85a31
yanchen01 <ychen01@bu.edu>
2020-09-26 01:00:51 -0400

    started frontend, completed navbar

---
+++ frontend/src/containers/Body/Body.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Body.module.css';
+
+import { Container } from 'react-bootstrap';
+
+const LandingPage = () => (
+	<Container className={styles.Body}>
+		<h2>Doubly</h2>
+	</Container>
+);
+
+export default LandingPage;
