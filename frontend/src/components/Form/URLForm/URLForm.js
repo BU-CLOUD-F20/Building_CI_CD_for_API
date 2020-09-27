e818e5e18b86342c3e49c22692b6bb205f51a4b2
yanchen01 <ychen01@bu.edu>
2020-09-27 02:15:11 -0400

    added display for short url and copy to clipboard feature

--- frontend/src/components/Form/URLForm/URLForm.js
+++ frontend/src/components/Form/URLForm/URLForm.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { Form, InputGroup, Col } from 'react-bootstrap';
 import Button from '../../Button/Button';
 
-const URLForm = () => {
+import styles from './URLForm.module.css';
+
+const URLForm = (props) => {
 	const [ validated, setValidated ] = useState(false);
 
 	const handleSubmit = (event) => {
@@ -15,16 +17,17 @@ const URLForm = () => {
 
 		const longURL = event.target.elements.URL.value;
 		console.log('Long URL: ', longURL);
-
 		setValidated(true);
+		props.getShortURLHandler(longURL);
+
 	};
 
 	return (
 		<Form noValidate validated={validated} onSubmit={handleSubmit}>
-			<Form.Row>
-				<Form.Group as={Col} md="6" controlId="URL">
-					<Form.Label>Enter URL</Form.Label>
-					<InputGroup>
+			<Form.Row className={styles.URLForm}>
+				<Form.Group as={Col} md="6" controlId="URL" className={styles.Form}>
+					<Form.Label className={styles.InputLabel}>Enter URL</Form.Label>
+					<InputGroup className={styles.InputField}>
 						<Form.Control
 							type="text"
 							placeholder="Paste URL here..."
@@ -34,11 +37,13 @@ const URLForm = () => {
 						<Form.Control.Feedback type="invalid">Please enter a valid URL.</Form.Control.Feedback>
 						<Form.Control.Feedback>Generating short URL...</Form.Control.Feedback>
 					</InputGroup>
+					<div className={styles.GetButton}>
+						<Button type="submit" color="#2789f2">
+							Get Shortened URL
+						</Button>
+					</div>
 				</Form.Group>
 			</Form.Row>
-			<Button type="submit" color="#2789f2">
-				Get Shortened URL
-			</Button>
 		</Form>
 	);
 };
--- frontend/src/containers/Body/Body.js
+++ frontend/src/containers/Body/Body.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Body.module.css';
 
 import { Container } from 'react-bootstrap';
 import URLForm from '../../components/Form/URLForm/URLForm';
+import ShortURLForm from '../../components/Form/ShortURLForm/ShortURLForm';
 
-const LandingPage = () => (
-	<Container className={styles.Body}>
-		<URLForm />
-	</Container>
-);
+const LandingPage = () => {
+	const [ shortURL, setShortURL ] = useState('');
+
+	const getShortURLHandler = (url) => {
+		console.log('Short URL Handler called on : ', url);
+		setShortURL(url);
+	};
+
+	return (
+		<Container className={styles.Body}>
+			<URLForm getShortURLHandler={getShortURLHandler} />
+			<ShortURLForm shortURL={shortURL} />
+		</Container>
+	);
+};
 
 export default LandingPage;
