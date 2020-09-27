747528ed04934b64cfd07550b883b31f58aad90b
yanchen01 <ychen01@bu.edu>
2020-09-27 02:15:11 -0400

    added display for short url and copy to clipboard feature

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const CustomButton = (props) => (
-	<Button style={{ backgroundColor: props.color }} type={props.type}>
+	<Button style={{ backgroundColor: props.color}} type={props.type} variant={props.variant}>
 		{props.children}
 	</Button>
 );
