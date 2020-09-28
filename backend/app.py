8e443d48cb54f949797695954be3716300e85d0d
SweetSourPeter <49703445+SweetSourPeter@users.noreply.github.com>
2020-09-28 19:43:19 +0800

    import error fixed

--- backend/app.py
+++ backend/app.py
@@ -20,4 +20,5 @@ initialize_routes(api)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    # app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, port=5000)
