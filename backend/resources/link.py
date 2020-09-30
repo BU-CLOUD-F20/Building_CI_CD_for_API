8a1eb34344c24399668656371e2d2e00273b8732
Mella Liang <mellaliang47@gmail.com>
2020-09-29 21:25:31 -1000

    Integrated api functions with MongoDB database

--- backend/resources/link.py
+++ backend/resources/link.py
@@ -1,70 +1,72 @@
 # Import flask
 from flask import redirect, jsonify, request
 from flask_restful import Resource
+from database.models import Link
 
 # Import libraries
 import string
+from datetime import date, datetime, timedelta
 
+# Constants
+WEBSITE_URL = 'http://localhost:5000/'
 
 class LinkApi(Resource):
     def get(self, link_id):
-        # get original URL from DB
-        exist = True  # check from DB if the short link exist in DB
-        if exist:
-            return redirect('https://www.youtube.com/')
-            # return redirect(original_link)
-        return {
-            'message': "The short link doesn't exist.",
-            'status': 404
-        }
+        try :
+            original_link = Link.objects.get_or_404(link_id=link_id)['original_link']
+            print('original_link', original_link)
+            return redirect(original_link)
+        except :
+            return 'Link not found', 400
 
     def post(self):
-        json_data = request.get_json(force=True)
-        # json data looks like this: (to be modified)
+        request_data = request.get_json()
+        original_link = request_data['original_link']
+        if 'expire_at' in request_data :
+            # add validation for date format later
+            expire_at = request_data['expire_at']
+        else :
+            # set default expire_at to 14 days from now
+            expire_at = (date.today() + timedelta(days=14)).strftime("%Y/%m/%d")
+        # request looks like this:
         # {
         #     "original_link": "https://www.youtube.com/",
-        #     "expire_at" : "2020/9/30"
+        #     "expire_at" : "2020/09/30"
         # }
-        original_link = json_data['original_link']
-        # checking URL validation
+
+        # find way to validate url
         try:
-            if 'http://' not in json_data['original_link'] or 'https://' not in json_data['original_link']:
+            if 'http://' not in original_link and 'https://' not in original_link :
                 original_link = 'http://' + original_link
-                print(original_link)
-            if '.com' not in json_data['original_link']:
-                original_link = original_link + '.com'
-                print(original_link)
-            response = request.args.get(original_link)
-            print("URL is valid and exists on the internet")
-            short_link = self.short_link_generator()  # call to the short link generator
-            # TODO
-            # Map short_link and json_data['original_link'] to DB
+            link_id = self.short_link_generator(original_link)
+            short_link = WEBSITE_URL + link_id
+            data = {
+                "original_link": original_link,
+                "expire_at": expire_at,
+                "short_link": short_link,
+                "link_id": link_id
+            }
+            Link(**data).save()
             return {
                 'short_link': short_link,
-                'expire_at': 'date',
-            }
-        except request.ConnectionError as exception:
-            return {
-                'message': json_data['original_link'] + " is not a valid URL",
-                'status': 404,
-            },
+                'expire_at': expire_at,
+            }, 201
+        except Exception as e :
+            print(e)
+            return 'Oops, something went wrong', 500
 
     def delete(self, link_id):
-        # TODO
-        # algo for deleting short_link
-        return 'deleted'
+        try :
+            Link.objects.get(link_id=link_id).delete()
+            return 'Deleted', 200
+        except :
+            return 'Link not found', 400
 
-    def short_link_generator(self):
-        # TODO
-        # Get INDEX of last url in the database. eg:
-        index = 990000099999  # change this index to see what the algo looks like
-        # index + 1 => the index of the new upcoming url
-        short_link = self.encode(index + 1)  # 62^5 choices
-        return short_link
-        # exist = False
-        # if exist:
-        #     return self.short_linkGenerator()
-        # return short_link
+    def short_link_generator(self, original_link):
+        # convert link to bytes then to int, grab only the last ten digits
+        link_as_int = int.from_bytes(original_link.encode("utf-8"), byteorder="big") % (10 ** 10)
+        link_id = self.encode(link_as_int)  # 62^5 choices
+        return link_id
 
     def encode(self, index):  # from number to 62 bases eg: 1000000 => 15FTGg
         characters = string.digits + string.ascii_letters
