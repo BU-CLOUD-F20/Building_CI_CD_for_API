f4df1ff0f42e5adfec8e5b28ad74bcc176a28231
Kaito Yamagishi <kaitoyamagishi1109@users.noreply.github.com>
2020-09-30 00:59:28 +0000

    Automated autoyapf fixes

--- resources/link.py
+++ resources/link.py
@@ -3,32 +3,33 @@ from flask import redirect, jsonify, request
 from flask_restful import Resource
 from database.models import Link
 from database.db import db
+
 # Import libraries
 import string
 from datetime import date, datetime, timedelta
 
 # Constants
-WEBSITE_URL = 'http://localhost:5000/'
+WEBSITE_URL = "http://localhost:5000/"
+
 
 class LinkAPI(Resource):
     def get(self, link_id):
-        try :
-            original_link = Link.objects.get_or_404(link_id=link_id)['original_link']
-            print('original_link', original_link)
+        try:
+            original_link = Link.objects.get_or_404(link_id=link_id)["original_link"]
+            print("original_link", original_link)
             # return redirect(original_link)
-            return jsonify(
-                original_link = original_link
-            )
-        except :
-            return 'Link not found', 400
+            return jsonify(original_link=original_link)
+        except:
+            return "Link not found", 400
+        return {"message": "The short link doesn't exist.", "status": 404}
 
     def post(self):
         request_data = request.get_json()
-        original_link = request_data['original_link']
-        if 'expire_at' in request_data :
+        original_link = request_data["original_link"]
+        if "expire_at" in request_data:
             # add validation for date format later
-            expire_at = request_data['expire_at']
-        else :
+            expire_at = request_data["expire_at"]
+        else:
             # set default expire_at to 14 days from now
             expire_at = (date.today() + timedelta(days=14)).strftime("%Y/%m/%d")
         # request looks like this:
@@ -39,53 +40,59 @@ class LinkAPI(Resource):
 
         # find way to validate url
         try:
-            if 'http://' not in original_link and 'https://' not in original_link :
-                original_link = 'http://' + original_link
-            link_id = self.short_link_generator(original_link)
-            short_link = WEBSITE_URL + link_id
-            data = {
-                "original_link": original_link,
-                "expire_at": expire_at,
+            if (
+                "http://" not in json_data["original_link"]
+                or "https://" not in json_data["original_link"]
+            ):
+                original_link = "http://" + original_link
+                print(original_link)
+            if ".com" not in json_data["original_link"]:
+                original_link = original_link + ".com"
+                print(original_link)
+            response = request.args.get(original_link)
+            print("URL is valid and exists on the internet")
+            short_link = self.short_link_generator()  # call to the short link generator
+            # TODO
+            # Map short_link and json_data['original_link'] to DB
+            return {
                 "short_link": short_link,
-                "link_id": link_id
+                "expire_at": "date",
             }
             Link(**data).save()
             response = jsonify(data)
-            response.status_code = 200 
+            response.status_code = 200
             return response
         # except db.errors.DuplicateKeyError:
-            
-        except Exception as e :
+
+        except Exception as e:
             try:
-                link_id = Link.objects.get_or_404(link_id=link_id)['link_id']
-                short_link = Link.objects.get_or_404(link_id=link_id)['short_link']
-                print('short_link', short_link)
-                return jsonify(
-                    short_link = short_link,
-                    message = 'the url alreadt exist'
-                ), 201
+                link_id = Link.objects.get_or_404(link_id=link_id)["link_id"]
+                short_link = Link.objects.get_or_404(link_id=link_id)["short_link"]
+                print("short_link", short_link)
+                return (
+                    jsonify(short_link=short_link, message="the url alreadt exist"),
+                    201,
+                )
             finally:
                 print(e)
-                return jsonify(
-                    link_id = link_id,
-                    short_link = short_link,
-                    message=str(e)
-                )
+                return jsonify(link_id=link_id, short_link=short_link, message=str(e))
                 # return 'Oops, something went wrong', 500
 
             # print(e)
             # return 'Oops, something went wrong', 500
 
     def delete(self, link_id):
-        try :
+        try:
             Link.objects.get(link_id=link_id).delete()
-            return 'Deleted', 200
-        except :
-            return 'Link not found', 400
+            return "Deleted", 200
+        except:
+            return "Link not found", 400
 
     def short_link_generator(self, original_link):
         # convert link to bytes then to int, grab only the last ten digits
-        link_as_int = int.from_bytes(original_link.encode("utf-8"), byteorder="big") % (10 ** 10)
+        link_as_int = int.from_bytes(original_link.encode("utf-8"), byteorder="big") % (
+            10 ** 10
+        )
         link_id = self.encode(link_as_int)  # 62^5 choices
         return link_id
 
@@ -97,4 +104,4 @@ class LinkAPI(Resource):
             val = index % base
             ret.append(characters[val])
             index = index // base
-        return ''.join(ret[::-1])
+        return "".join(ret[::-1])
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,157 +10,167 @@ def test_base_route():
     app = Flask(__name__)
     api = Api(app, catch_all_404s=True)
     # Config
-    app.config['MONGODB_SETTINGS'] = {
-        'host': 'mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority'
+    app.config["MONGODB_SETTINGS"] = {
+        "host": "mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority"
     }
 
     initialize_db(app)
     initialize_routes(api)
     client = app.test_client()
-    url ='/'
+    url = "/" + "aaaa"
 
     response = client.get(url)
-    assert response.headers[1] == ('Content-Length', '37')
-    # assert response.headers[2] == ('Location', 'https://www.youtube.com/')
-    assert response.status_code == 500 
+    assert response.headers[2] == ("Location", "https://www.youtube.com/")
+    assert (
+        response.status_code == 302
+    )  # redirect respond code to 'https://www.youtube.com/'
+
+
+# import json
 
 
 def test_None_Existing_route():
     app = Flask(__name__)
     api = Api(app, catch_all_404s=True)
     # Config
-    app.config['MONGODB_SETTINGS'] = {
-        'host': 'mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority'
+    app.config["MONGODB_SETTINGS"] = {
+        "host": "mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority"
     }
 
     initialize_db(app)
     initialize_routes(api)
     client = app.test_client()
 
-    url ='/testNonExist'
+    url = "/testNonExist"
     response = client.get(url)
-    assert response.headers[1] == ('Content-Length', '17')
+    assert response.headers[1] == ("Content-Length", "17")
     # assert response.headers[2] == ('Location', 'https://www.youtube.com/')
     assert response.status_code == 400
 
-#post a new test url1
+
+# post a new test url1
 def test_case1():
     app = Flask(__name__)
     api = Api(app, catch_all_404s=True)
     # Config
-    app.config['MONGODB_SETTINGS'] = {
-        'host': 'mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority'
+    app.config["MONGODB_SETTINGS"] = {
+        "host": "mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority"
     }
 
     initialize_db(app)
     initialize_routes(api)
     client = app.test_client()
-    url ='/'
+    url = "/"
     test_data = {
-	    "original_link": "https://www.UrlUnitTesting1.com/",
-	    "expire_at" : "2020-9-30"
+        "original_link": "https://www.UrlUnitTesting1.com/",
+        "expire_at": "2020-9-30",
     }
-    #post a new test url1
-    response = client.post('/', 
-                       data=json.dumps(test_data),
-                       content_type='application/json')
-                       
+    # post a new test url1
+    response = client.post(
+        "/", data=json.dumps(test_data), content_type="application/json"
+    )
+
     # data = response.json()
     # (url, data = test_data)
     # assert response['expire_at'] == "2020-9-30"
     # assert response.headers[2] == ('Location', 'https://www.youtube.com/')
     assert response.status_code == 200
 
-
-    link_id = response.json['link_id']
-    response = client.get('/' + link_id)    
-    assert response.status_code == 200 
-
+    link_id = response.json["link_id"]
+    response = client.get("/" + link_id)
+    assert response.status_code == 200
 
 
-#post a new test url2
+# post a new test url2
 def test_case2():
     app = Flask(__name__)
     api = Api(app, catch_all_404s=True)
     # Config
-    app.config['MONGODB_SETTINGS'] = {
-        'host': 'mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority'
+    app.config["MONGODB_SETTINGS"] = {
+        "host": "mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority"
     }
 
     initialize_db(app)
     initialize_routes(api)
     client = app.test_client()
     test_data = {
-	    "original_link": "https://www.UrlUnitTesting2.com/",
-	    "expire_at" : "2020-10-28"
+        "original_link": "https://www.UrlUnitTesting2.com/",
+        "expire_at": "2020-10-28",
     }
-    #post a new test url1
-    response = client.post('/', 
-                       data=json.dumps(test_data),
-                       content_type='application/json')
-                       
+    # post a new test url1
+    response = client.post(
+        "/", data=json.dumps(test_data), content_type="application/json"
+    )
+
     # data = response.json()
     # (url, data = test_data)
     # assert response['expire_at'] == "2020-10-28"
     # assert response.headers[2] == ('Location', 'https://www.youtube.com/')
-    assert response.status_code == 200 
+    assert response.status_code == 200
+
+    link_id = response.json["link_id"]
+    response = client.get("/" + link_id)
+    assert response.status_code == 200
+
+    response = client.delete("/" + link_id)
+    assert response.status_code == 200
 
 
-    link_id = response.json['link_id']
-    response = client.get('/' + link_id)    
-    assert response.status_code == 200 
-
-    response = client.delete('/' + link_id)
-    assert response.status_code == 200 
-
-#post a new test url2
+# post a new test url2
 def test_case3():
     app = Flask(__name__)
     api = Api(app, catch_all_404s=True)
     # Config
-    app.config['MONGODB_SETTINGS'] = {
-        'host': 'mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority'
+    app.config["MONGODB_SETTINGS"] = {
+        "host": "mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority"
     }
 
     initialize_db(app)
     initialize_routes(api)
     client = app.test_client()
     test_data = {
-	    "original_link": "https://www.UrlUnitTesting3.com/",
-	    "expire_at" : "2020-10-28"
+        "original_link": "https://www.UrlUnitTesting3.com/",
+        "expire_at": "2020-10-28",
     }
-    #post a new test url1
-    response = client.post('/', 
-                       data=json.dumps(test_data),
-                       content_type='application/json')
-                       
+    # post a new test url1
+    response = client.post(
+        "/", data=json.dumps(test_data), content_type="application/json"
+    )
+
     # data = response.json()
     # (url, data = test_data)
     # assert response['expire_at'] == "2020-10-28"
     # assert response.headers[2] == ('Location', 'https://www.youtube.com/')
-    assert response.status_code == 200 
+    assert response.status_code == 200
+
+    link_id = response.json["link_id"]
+    response = client.get("/" + link_id)
+    assert response.status_code == 200
+
+    response = client.delete("/" + link_id)
+    assert response.status_code == 200
 
 
-    link_id = response.json['link_id']
-    response = client.get('/' + link_id)    
-    assert response.status_code == 200 
-
-    response = client.delete('/' + link_id)
-    assert response.status_code == 200 
-
-#delete testing non-exist
+# delete testing non-exist
 def test_delete_nonexisting_case():
     app = Flask(__name__)
     api = Api(app, catch_all_404s=True)
     # Config
-    app.config['MONGODB_SETTINGS'] = {
-        'host': 'mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority'
+    app.config["MONGODB_SETTINGS"] = {
+        "host": "mongodb+srv://cicd-team:ec528@cicd-cluster0.s9vur.gcp.mongodb.net/doubly?retryWrites=true&w=majority"
     }
 
     initialize_db(app)
     initialize_routes(api)
     client = app.test_client()
-    response = client.delete("/ + ""testNonExist")
-    assert response.headers[1] == ('Content-Length', '17')
+    response = client.delete("/ + " "testNonExist")
+    assert response.headers[1] == ("Content-Length", "17")
     assert response.status_code == 400
-#delete testing exist
+
+
+# delete testing exist
+# def test_index(app, client):
+#     res = client.get('/')
+#     assert res.status_code == 200
+#     expected = {'hello': 'world'}
+#     assert expected == json.loads(res.get_data(as_text=True))
