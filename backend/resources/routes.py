510bd548b9969d2298a535cc0df32b19df520266
yanchen01 <ychen01@bu.edu>
2020-10-12 14:24:08 -0400

    fixed endpoint conflicts and updated test

--- backend/resources/routes.py
+++ backend/resources/routes.py
@@ -1,6 +1,5 @@
-from .link import LinkAPI, BaseAPI
+from .link import LinkAPI
 
 
 def initialize_routes(api):
-    api.add_resource(BaseAPI, '/')
-    api.add_resource(LinkAPI, '/', '/<string:link_id>')
+    api.add_resource(LinkAPI, '/api/', '/<string:link_id>')
